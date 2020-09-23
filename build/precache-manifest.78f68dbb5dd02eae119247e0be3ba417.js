3580b5930b74d552c1ce7e5fbe55d3331c2614c6
ASentientBanana <asentientbanana@gmail.com>
2020-09-22 18:30:02 -0700

    added build folder

---
+++ build/precache-manifest.78f68dbb5dd02eae119247e0be3ba417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9304db66b35a7420cfb683761f3a67d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ed6135b56ea79156fb",
+    "url": "/static/css/2.3a13669e.chunk.css"
+  },
+  {
+    "revision": "e03fc9ecfd56bfc72eb8",
+    "url": "/static/css/main.ea8b44c5.chunk.css"
+  },
+  {
+    "revision": "f3ed6135b56ea79156fb",
+    "url": "/static/js/2.8d3b7938.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/static/js/2.8d3b7938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03fc9ecfd56bfc72eb8",
+    "url": "/static/js/main.bd63f2ec.chunk.js"
+  },
+  {
+    "revision": "a0003e65cf28c585feb8",
+    "url": "/static/js/runtime-main.6ecb8fcc.js"
+  }
+]);
