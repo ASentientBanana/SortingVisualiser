fe11a24747705a7eb1fe18b4cd8038b32f39a46a
ASentientBanana <asentientbanana@gmail.com>
2020-12-30 10:24:14 +0100

    fixed some buggs and  finished a couple of unfinished things

--- src/algorithms/SortingUtility.ts
+++ src/algorithms/SortingUtility.ts
@@ -1,71 +1,130 @@
-import React from 'react'
+import React from "react";
 
 class SortingUtility {
+  static sleep(time: number = 0) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+  static swap(
+    array: number[],
+    i: number,
+    j: number,
+    updateColor: Function,
+    updateColor2: Function
+  ) {
+    updateColor(i);
+    updateColor2(j);
+    let tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+  }
 
-    static sleep(time: number = 0) {
-        return new Promise(resolve => setTimeout(resolve, time));
-    }
-    static swap(array: number[], i: number, j: number, updateColor: Function, updateColor2: Function) {
-        updateColor(i);
-        updateColor2(j);
-        let tmp = array[i]
-        array[i] = array[j];
-        array[j] = tmp;
-    }
-
-    static async bubbleSort(array: number[], updateArray: Function, updateColor: Function, updateColor2: Function) {
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 0; j < array.length; j++) {
-                if (array[i] < array[j]) {
-                    await this.sleep(10);
-                    SortingUtility.swap(array, i, j, updateColor, updateColor2);
-                    updateArray([...array])
-                }
-            }
+  static async bubbleSort(
+    array: number[],
+    updateArray: Function,
+    updateColor: Function,
+    updateColor2: Function
+  ) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length; j++) {
+        if (array[i] < array[j]) {
+          await this.sleep(2);
+          SortingUtility.swap(array, i, j, updateColor, updateColor2);
+          updateArray([...array]);
         }
-        return array;
+      }
     }
+    return array;
+  }
 
-    static partition = async (arr: number[], pivot: number, left: number, right: number, updateArray: Function, setCurrNum1: Function, setCurrNum2: Function) => {
-        let pivotValue = arr[pivot],
-            partitionIndex = left;
-        for (let i = left; i < right; i++) {
-            if (arr[i] < pivotValue) {
-                SortingUtility.swap(arr, i, partitionIndex, setCurrNum1, setCurrNum2);
-                if (updateArray) updateArray([... await arr])
-                await SortingUtility.sleep(10)
-                partitionIndex++;
-            }
-        }
-        SortingUtility.swap(arr, right, partitionIndex, setCurrNum1, setCurrNum2);
-        return partitionIndex;
+  static partition = async (
+    arr: number[],
+    pivot: number,
+    left: number,
+    right: number,
+    updateArray: Function,
+    setCurrNum1: Function,
+    setCurrNum2: Function
+  ) => {
+    let pivotValue = arr[pivot],
+      partitionIndex = left;
+    for (let i = left; i < right; i++) {
+      if (arr[i] < pivotValue) {
+        SortingUtility.swap(arr, i, partitionIndex, setCurrNum1, setCurrNum2);
+        if (updateArray) updateArray([...(await arr)]);
+        await SortingUtility.sleep(2);
+        partitionIndex++;
+      }
     }
+    SortingUtility.swap(arr, right, partitionIndex, setCurrNum1, setCurrNum2);
+    return partitionIndex;
+  };
 
-    static quickSort = async (arr: number[], left: number, right: number, updateArray: Function, setCurrNum1: Function, setCurrNum2: Function) => {
-        if (left < right) {
-            const pivot = right;
+  static quickSort = async (
+    arr: number[],
+    left: number,
+    right: number,
+    updateArray: Function,
+    setCurrNum1: Function,
+    setCurrNum2: Function,
+    cnt: number
+  ) => {
+    if (left < right) {
+      const pivot = right;
 
-            const partitionIndex: number = await SortingUtility.partition(arr, pivot, left, right, updateArray, setCurrNum1, setCurrNum2);
-            await Promise.all([SortingUtility.quickSort(arr, left, partitionIndex - 1, updateArray, setCurrNum1, setCurrNum2), SortingUtility.quickSort(arr, partitionIndex + 1, right, updateArray, setCurrNum1, setCurrNum2)])
-        }
-        return await arr;
+      const partitionIndex: number = await SortingUtility.partition(
+        arr,
+        pivot,
+        left,
+        right,
+        updateArray,
+        setCurrNum1,
+        setCurrNum2
+      );
+      await Promise.all([
+        SortingUtility.quickSort(
+          arr,
+          left,
+          partitionIndex - 1,
+          updateArray,
+          setCurrNum1,
+          setCurrNum2,
+          cnt++
+        ),
+        SortingUtility.quickSort(
+          arr,
+          partitionIndex + 1,
+          right,
+          updateArray,
+          setCurrNum1,
+          setCurrNum2,
+          cnt++
+        ),
+      ]);
     }
+    if (cnt === 0) console.log("done");
 
-    static selectionSort = async (array: number[], updateArray: Function, setCurrNum1: Function, setCurrNum2: Function) => {
-        for (let i = 0; i < array.length; i++) {
-            //set min to the current iteration of i
-            let min = i;
-            for (let j = i + 1; j < array.length; j++) {
-                if (array[j] < array[min]) {
-                    min = j;
-                }
-            }
-            SortingUtility.swap(array, i, min, setCurrNum1, setCurrNum2);
-            await SortingUtility.sleep(10);
-            updateArray([...array])
+    return await arr;
+  };
+
+  static selectionSort = async (
+    array: number[],
+    updateArray: Function,
+    setCurrNum1: Function,
+    setCurrNum2: Function
+  ) => {
+    for (let i = 0; i < array.length; i++) {
+      //set min to the current iteration of i
+      let min = i;
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < array[min]) {
+          min = j;
         }
-        return array;
-    };
-
+      }
+      SortingUtility.swap(array, i, min, setCurrNum1, setCurrNum2);
+      await SortingUtility.sleep(2);
+      updateArray([...array]);
+    }
+    return array;
+  };
 }
-export default SortingUtility
+export default SortingUtility;
--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,85 +1,157 @@
-import React, { useEffect, createRef, useState } from 'react';
-import './Canvas.css';
-import SortingUtility from '../../algorithms/SortingUtility'
+import React, {
+  useEffect,
+  createRef,
+  useState,
+  useCallback,
+  useRef,
+} from "react";
+import "./Canvas.css";
+import SortingUtility from "../../algorithms/SortingUtility";
 interface canvas {
-    alg: string
-    arrayState: any
+  alg: string;
+  arrayState: any;
 }
 
 const Canvas = ({ alg, arrayState }: canvas) => {
-    const canvasRef = createRef<HTMLCanvasElement>();
-    const [currNum, setCurrNum] = useState(0);
-    const [currNum2, setCurrNum2] = useState(0);
-    const [array, setArray] = arrayState;
-    //#region  Canvas Draw
-    const canvasSize = (canvasProp: HTMLCanvasElement) => {
-        canvasProp.height = window.innerHeight * 0.6;
-        canvasProp.width = window.innerWidth * 0.6;
-        return { x: canvasProp.width, y: canvasProp.height }
+  const canvasRef = createRef<HTMLCanvasElement>();
+  const [currNum, setCurrNum] = useState(0);
+  const [currNum2, setCurrNum2] = useState(0);
+  const [array, setArray] = arrayState;
+  const clickState = useRef(true);
+  const sortedState = useRef(false);
+  const randArr = useRef([...array]);
+  const [rend, setRend] = useState(false);
+
+  //#region  Canvas Draw
+  const canvasSize = (canvasProp: HTMLCanvasElement) => {
+    canvasProp.height = window.innerHeight * 0.6;
+    canvasProp.width = window.innerWidth * 0.6;
+    return { x: canvasProp.width, y: canvasProp.height };
+  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const drawBars = (
+    ctx: CanvasRenderingContext2D,
+    canvasSize: any,
+    randomArray: number[],
+    barSizeNormalizer: number
+  ) => {
+    const count = randomArray.length;
+    const barWidth = canvasSize.x / count;
+    for (let i = 0; i < count; i++) {
+      if (i === currNum) {
+        ctx.fillStyle = "red";
+      } else if (i === currNum2) {
+        ctx.fillStyle = "green";
+      } else {
+        ctx.fillStyle = "#eab354";
+      }
+
+      ctx.fillRect(
+        i * barWidth,
+        canvasSize.y,
+        barWidth,
+        randomArray[i] * -barSizeNormalizer
+      );
     }
-    const drawBars = (ctx: CanvasRenderingContext2D, canvasSize: any, randomArray: number[], barSizeNormalizer: number) => {
-        const count = randomArray.length;
-        const barWidth = canvasSize.x / count;
-        for (let i = 0; i < count; i++) {
-            if (i === currNum) {
-                ctx.fillStyle = 'red';
+  };
+  const draw = useCallback(() => {
+    if (canvasRef.current) {
+      const canvasContext = canvasRef.current.getContext("2d");
+      const sizeOfCanvas = canvasSize(canvasRef.current);
+      const barSizeNormalizer = editBarHeight(array, canvasRef.current);
+      if (canvasContext)
+        drawBars(canvasContext, sizeOfCanvas, array, barSizeNormalizer);
+    }
+  }, [array, canvasRef, drawBars]);
+  const editBarHeight = (randomArray: number[], canvas: HTMLCanvasElement) => {
+    let heighestNumber = 0;
+    for (let i = 0; i < randomArray.length; i++) {
+      if (randomArray[i] > heighestNumber) heighestNumber = randomArray[i];
+    }
+    return canvas.height / heighestNumber;
+  };
+  //#endregion
+
+  const algSelect = () => {
+    if (sortedState.current === true) {
+      setArray(randArr.current);
+    }
+    clickState.current = false;
+    switch (alg) {
+      case "bubble":
+        console.log(alg);
+        Promise.resolve(
+          SortingUtility.bubbleSort(array, setArray, setCurrNum, setCurrNum2)
+        ).then((res) => {
+          clickState.current = true;
+          sortedState.current = true;
+          setCurrNum(-1);
+          setCurrNum2(-1);
+          draw();
+        });
+        break;
+      case "quick":
+        console.log(alg);
+        let cnt = 0;
+        Promise.resolve(
+          SortingUtility.quickSort(
+            array,
+            0,
+            array.length - 1,
+            setArray,
+            setCurrNum,
+            setCurrNum2,
+            cnt
+          )
+        ).then((res) => {
+          clickState.current = true;
+          sortedState.current = true;
+          setCurrNum(-1);
+          setCurrNum2(-1);
+          draw();
+        });
+        break;
+      case "selection":
+        console.log(alg);
+        Promise.resolve(
+          SortingUtility.selectionSort(array, setArray, setCurrNum, setCurrNum2)
+        ).then((res) => {
+          clickState.current = true;
+          sortedState.current = true;
+          setCurrNum(-1);
+          setCurrNum2(-1);
+          draw();
+        });
+
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    if (canvasRef.current != null) draw();
+  }, [canvasRef]);
+  return (
+    <div className="canvas-container">
+      <canvas
+        ref={canvasRef}
+        className="canvas"
+        onClick={() => {
+          if (clickState.current) {
+            if (sortedState.current) {
+              setArray(randArr.current);
+              draw();
+              sortedState.current = false;
+            } else {
+              algSelect();
             }
-            else if (i === currNum2) {
-                ctx.fillStyle = 'green';
-            }
-            else {
-                ctx.fillStyle = '#eab354';
-            }
+          }
+        }}
+      ></canvas>
+      <h3 className="title-text">Click to see {alg} sort</h3>
+    </div>
+  );
+};
 
-            ctx.fillRect(i * barWidth, canvasSize.y, barWidth, randomArray[i] * -barSizeNormalizer);
-        }
-    }
-    const editBarHeight = (randomArray: number[], canvas: HTMLCanvasElement) => {
-        let heighestNumber = 0;
-        for (let i = 0; i < randomArray.length; i++) {
-            if (randomArray[i] > heighestNumber) heighestNumber = randomArray[i]
-        }
-        return canvas.height / heighestNumber;
-    }
-    //#endregion 
-
-    const algSelect = () => {
-        switch (alg) {
-            case "bubble":
-                console.log(alg);
-                // bubbleSort(array);
-                SortingUtility.bubbleSort(array, setArray, setCurrNum, setCurrNum2)
-                break;
-            case "quick":
-                console.log(alg);
-                SortingUtility.quickSort(array, 0, array.length - 1, setArray, setCurrNum, setCurrNum2)
-                break
-            case "selection":
-                console.log(alg);
-                SortingUtility.selectionSort(array, setArray, setCurrNum, setCurrNum2);
-                break;
-            default:
-                break;
-        }
-    }
-
-    useEffect(() => {
-        if (canvasRef.current != null) {
-            const canvasContext = canvasRef.current.getContext("2d");
-            const sizeOfCanvas = canvasSize(canvasRef.current);
-            const barSizeNormalizer = editBarHeight(array, canvasRef.current)
-            if (canvasContext != null) drawBars(canvasContext, sizeOfCanvas, array, barSizeNormalizer)
-        }
-    }, [array, canvasRef, drawBars]);
-    return (
-        <div className='canvas-container'>
-            <canvas ref={canvasRef} className='canvas' onClick={() => {
-                algSelect();
-            }}>
-            </canvas>
-            <h3 className="title-text">Click to see {alg} sort</h3>
-        </div>
-    )
-}
-
-export default Canvas
+export default Canvas;
